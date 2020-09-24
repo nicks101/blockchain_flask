16796c0a4659ade37ba2ce403cb6437c7e6c1605
Nikki Goel <gupta.nikkigupta@gmail.com>
2020-09-24 16:36:38 +0530

    refactor and added doc strings

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,16 +1,30 @@
+# Initializing blockchain list
 blockchain = []
 
 
 def get_last_blockchain_value():
+    """ Returns the last value of current blockchain. """
     return blockchain[-1]
 
 
 def add_value(transaction_amount, last_transaction=[1]):
+    """ Append new value as well as the last blockchain to the blockchain.
+
+    Arguments:
+        :transaction_amount: The amount that would be added to the blockchain.
+        :last_transaction: The last blockchain transaction (default [1]).
+
+    """
     blockchain.append([last_transaction, transaction_amount])
 
 
-add_value(2)
-add_value(3, get_last_blockchain_value())
-add_value(4, get_last_blockchain_value())
+def get_user_input():
+    """ Returns the input of the user (a new transaction amount) as a float. """
+    return float(input("Your transaction amount : "))
+
+
+add_value(get_user_input())
+add_value(get_user_input(), get_last_blockchain_value())
+add_value(get_user_input(), get_last_blockchain_value())
 
 print(blockchain)
